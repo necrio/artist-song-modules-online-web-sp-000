5f5a061ac95bbc0cf6ebbcf3628a4d03415939d7
Keith Isham <kisham8899@gmail.com>
2019-05-23 14:35:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,9 +11,9 @@ extend Memorable::ClassMethods
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
