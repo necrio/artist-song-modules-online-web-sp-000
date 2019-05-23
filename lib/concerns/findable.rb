2bdbce9d254094559982be03fd5832d483800a47
Keith Isham <kisham8899@gmail.com>
2019-05-23 14:34:24 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -3,11 +3,11 @@
 
 
 module findable
-            module ClassMethods
+        module ClassMethods
               def find_by_name(name)
                 self.all.detect{|all| all.name}
               end
-            end
+        end
 
 
 
