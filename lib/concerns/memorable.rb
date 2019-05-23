dfc34a291cec1aacd9c30411d470960a0fc2a1ae
Keith Isham <kisham8899@gmail.com>
2019-05-23 14:29:31 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -2,21 +2,22 @@
 
 
 module Memorable
-  module ClassMethods
-    def reset_all
-      self.all.clear
-    end
+    module ClassMethods
+              def reset_all
+                self.all.clear
+              end
 
-    def count
-      self.all.count
-    end
+              def count
+                self.all.count
+              end
+      end
 
-  module InstanceMethods
+        module InstanceMethods
 
-    def initialize
-      self.class.all << self
-    end
-  end
+                  def initialize
+                    self.class.all << self
+                  end
+        end
 
 
 
