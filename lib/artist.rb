fb18354f510eb6763b2413830a31275b5e2de6cf
Keith Isham <kisham8899@gmail.com>
2019-05-23 14:40:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,9 +12,9 @@ extend Findable::ClassMethods
     @songs = []
   end
 
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
+  def self.find_by_name(name)
+    @@artists.detect{|a| a.name == name}
+  end
 
   def self.all
     @@artists
